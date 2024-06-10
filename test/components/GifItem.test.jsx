d5c6ec132fbef1d80fe9405c73c300bdba483a0f
EtnofoodSystem <sistemas@etnofood.org>
2024-06-10 16:23:50 -0600

    random

--- test/components/GifItem.test.jsx
+++ test/components/GifItem.test.jsx
@@ -1,6 +1,10 @@
-describe('', () => {
+import { render } from '@testing-library/react'
+import { GifItem } from '../../src/components'
 
-    test('',() => {
+describe('Pruebas en <GifItem />', () => {
+    
 
+    test('Debe de hacer match con el snapshot',() => {
+          render( GifItem );
     })
 })
