d7ba354291561a763bd5593c584a8dc416c46c0f
FrancoFabian <jeralfabian489@gmail.com>
2024-06-10 22:30:59 -0600

    Se agregaron test de getGifs

--- test/components/GifItem.test.jsx
+++ test/components/GifItem.test.jsx
@@ -1,10 +1,23 @@
-import { render } from '@testing-library/react'
+import { render,screen } from '@testing-library/react'
 import { GifItem } from '../../src/components'
-
+import React from 'react'
 describe('Pruebas en <GifItem />', () => {
-    
-
+    const title = "One Punch";
+    const url = 'http://one-punch.com/saitma.jpg';
     test('Debe de hacer match con el snapshot',() => {
-          render( GifItem );
-    })
+          const { container } = render( <GifItem title={ title } url={ url }/> );
+          expect( container ).toMatchSnapshot();
+        })
+
+    test('debe mostrar la imagen con la url inidicad', () => {
+        render(<GifItem title={ title } url={ url }/>);
+        //expect( screen.getByRole('img').src).toBe(url);
+        const {src, alt} = screen.getByRole('img')
+        expect( src ).toBe( url );
+        expect( alt ).toBe( alt );
+    });
+    test('Debe mostrar el titulo en el componente', () => {
+        render(<GifItem title={ title } url={ url }/>);
+        expect( screen.getByText( title )).toBeTruthy()
+    });
 })
